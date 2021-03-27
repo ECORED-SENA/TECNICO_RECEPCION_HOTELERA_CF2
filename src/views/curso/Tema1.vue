4dd3b6d671607d98c07cdd3485dc34e771374b42
elrilacho <80546125+elrilacho@users.noreply.github.com>
2021-03-27 11:06:45 -0500

    CF2 V0.2

--- src/views/curso/Tema1.vue
+++ src/views/curso/Tema1.vue
@@ -3,7 +3,7 @@
   BannerInterno(titulo="1. Servicios Turísticos")
   .container.tarjeta--blanca.p-4.p-md-5.mb-5
     .imagen.mt-5
-      img(src="@/assets/curso/tema-1/img1.svg")
+      img(src="@/assets/curso/tema-1/jpg/img1.jpg")
     .row.mt-5
       .col-lg-6.col-md-12
         p.mt-5 Al hablar de servicios turísticos nos referimos a los bienes y servicios ofrecidos por las empresas del mercado turístico y que satisfacen las necesidades de los turistas en la organización de viajes y mediante el disfrute de estos; 
@@ -13,7 +13,112 @@
         p El turismo se fundamenta en un sistema de relaciones entre el sujeto (turista) y el objeto (destino, prestadores, organización). Para el turista, el turismo es el conjunto de servicios que le son facilitados por los prestadores de servicios turísticos y para estos el turista es el destinatario de los servicios que ofrece.
       .col-lg-6.col-md-12
         .imagen.bt-5
-          img(src="@/assets/curso/tema-1/img2.svg")
+          img(src="@/assets/curso/tema-1/svg/img1.svg")
+    div.rp.mt-5 
+          img.img-rp(src="@/assets/curso/tema-1/svg/img2.svg")
+          p 
+            b Servicios turísticos: 
+            b-none es el conjunto de actividades perfectamente diferenciadas entre sí, pero íntimamente relacionadas, que funcionan en forma armónica y coordinada con el propósito de responder a las exigencias de servicios que, en un destino, generan  una corriente turística.
+    p.mt-5 Este va de la mano con el conjunto de interacciones humanas, 
+      b como transportes, hospedaje, alimentación, diversiones, aprendizajes, derivados de los desplazamientos transitorios, temporales o de transeúntes 
+      b-none de fuertes núcleos  de población con propósitos diversos y múltiples deseos humanos, que abarcan una gama amplia  de motivaciones de viaje.
+    br
+    p El servicio turístico comprende el conjunto de técnicas relativas a la transferencia y venta de bienes turísticos del productor al consumidor.  Constituye la política de entidades turísticas en relación con 
+      b la oferta, la demanda y la preparación del producto turístico para la venta, 
+      b-none distribución, comercialización  y satisfacción óptima de necesidades de un segmento determinado de consumidores (Novas, 2006).
+    h4 Servicios turísticos:
+    div.div-box-general
+      div.div-box
+        img.img-box(src="@/assets/curso/tema-1/svg/img3.svg")
+        h6.text-center.mt-3 Tour y excursiones
+      div.div-box
+        img.img-box(src="@/assets/curso/tema-1/svg/img4.svg")
+        h6.text-center.mt-3 Transporte
+      div.div-box
+        img.img-box(src="@/assets/curso/tema-1/svg/img5.svg")
+        h6.text-center.mt-3 Alojamiento
+      div.div-box
+        img.img-box(src="@/assets/curso/tema-1/svg/img6.svg")
+        h6.text-center.mt-3 Agencia de viajes
+      div.div-box
+        img.img-box(src="@/assets/curso/tema-1/svg/img7.svg")
+        h6.text-center.mt-3 Restaurantes
+    .row.mt-5
+      .col-lg-8
+        p Para la prestación de servicios turísticos es necesario conocer el funcionamiento del sistema turístico, que se define como el conjunto de elementos interrelacionados entre sí que evolucionan de forma dinámica para configurar la naturaleza de la actividad turística, conformado por cinco  subsistemas:  
+      .col-lg-auto
+        ul
+          li Estructura turística (planta turística).
+          li Infraestructura turística.
+          li Superestructura turística.
+          li Oferta turística.
+          li Demanda turística.
+    
+                //-  // AQUI HACE FALTA UN CAROUSEL
+    p.mt-5   Producto turístico lo integra la sumatoria de los  atractivos turísticos, los servicios de la planta turística y la accesibilidad dentro de un destino, que integrados logran satisfacer las necesidades y motivaciones del turista.
+    br
+    |Todos estos elementos que conforman el producto turístico se resumen en tres:
+    .row.mt-5
+      .col-lg-7
+        h4 Atractivos turísticos
+        p Son todos aquellos elementos naturales, bienes creados por la mano del hombre y hechos sociales que crean el interés de ser conocidos y permiten su disfrute.
+      .col-lg-5
+        img.img-box(src="@/assets/curso/tema-1/jpg/img2.jpg")
+    .row.mt-3
+      .col-lg-5
+        img.img-box(src="@/assets/curso/tema-1/jpg/img3.jpg")
+      .col-lg-7
+        h4 Servicios turísticos o planta turística
+        p Son el conjunto de medios y actividades que facilitan la estancia y el bienestar del viajero, convergiendo en ellos elementos tangibles e intangibles, los cuales son en su etapa final de consumo, interdependientes (Instituto Distrital de Turismo ANATO Asociación Colombiana de Agencias de Viajes y Turismo, 2007).
+    .row.mt-3
+      .col-lg-7
+        h4 accesibilidad
+        p La accesibilidad en el diseño de producto hace relación solamente a las vías de comunicación que se dispone para acceder al destino y a cada uno de los lugares de interés.
+      .col-lg-5
+        img.img-box(src="@/assets/curso/tema-1/jpg/img4.jpg")
+
+    div.table-general.d-block.m-auto.mt-5
+      div.table-title
+        h4 Producto turístico
+      div.table-columns
+        div.column-1
+          h5.text-center Atractivos turísticos.
+          ul
+            li Sitios naturales.
+            li Manifestaciones culturales.
+            li Hechos históricos.
+            li Museos.
+            li Folclor.
+            li Acontecimientos programados - eventos.
+            li Realizaciones técnicas, cientificas o artísticas contemporanea.
+        div.column-2
+          h5.text-center Servicios turísticos o planta turística.
+          ul 
+            li Alojamiento
+            li Restauración.
+            li Esparcimiento y diversión.
+            li Agencias de viajes.
+            li Guías de turismo.
+        div.column-1
+          h5.text-center Accesibilidad.
+          ul
+            li Transporte
+            li Comunicación.
+            li Servicios Públicos.
+    div.image-text.mt-5
+      img.img-text(src="@/assets/curso/tema-1/jpg/img5.jpg")
+      div.text-image
+        h1.h1-video.d-none.d-xl-block Video Colombia
+        h2.h2-video.d-none.d-xl-block Land Of Sabrosura 
+        div.video-enlace
+          a(href="https://www.youtube.com/watch?v=JNDGMhTo9e8",target="_blank")
+            img.img-rp(src="@/assets/curso/tema-1/svg/img8.svg")
+            p
+              b Mira el video
+              br
+              | Esto es un boton que abre en un modal el video 
+              
+
 
     #t_1_1.pb-5.h2 tema 1.1
       
